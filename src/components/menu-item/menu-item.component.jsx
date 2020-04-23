aef203e39c7ea723e61fc6f5ceb2a2cac4fbbcb9
Kush Singh <kush.singh84@gmail.com>
2020-04-24 00:05:18 +0530

    update our menu items to navigate withRouter HOC (Higher Order components)

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => ( // Object destructuring i ES6
-    <div className={`${size} menu-item`}>
-    <div className="background-image" style=  {{
-        backgroundImage: `url(${imageUrl})`
-    }} />
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => ( // Object destructuring i ES6
+    <div className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+        <div 
+            className="background-image" 
+            style=  {{
+                backgroundImage: `url(${imageUrl})`
+            }} 
+        />
         <div className="content">
             <h1 className="title">{ title.toUpperCase() }</h1>
             <span className="subtitle">SHOP NOW</span>
@@ -14,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => ( // Object destructuring i ES6
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,9 +5,9 @@ import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
 
 
-const HomePage = () => (
+const HomePage = ({ history }) => (
     <div className="homepage">
-        <Directory />
+        <Directory history={history} />
     </div>
 );
 
