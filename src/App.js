75e82afeff584c30ef81340bbda40e9d83ebdde2
Kush Singh <kush.singh84@gmail.com>
2020-05-02 01:18:33 +0530

    implemented firebase utils includesd ability to store authentication userinto our firebase DB

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage  from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SigninAndRegister from './pages/signin-n-register/signin-n-register.component'
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
  
 
 class App extends React.Component{
@@ -19,12 +19,31 @@ class App extends React.Component{
         }
     }
 
-    componentDidMount() {
-        auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user });
+    unsubscribeFromAuth = null;
 
-            console.log(user)
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot (snapshot => {
+                    this.setState ({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    }, () => {
+                        console.log(this.state);
+                    })
+                })
+                
+            }
+            this.setState({ currentUser : userAuth })
         });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
     }
 
     render() {
--- src/pages/signin-n-register/signin-n-register.component.jsx
+++ src/pages/signin-n-register/signin-n-register.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import  SignIn from '../../components/sign-in/sign-in.components';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './signin-n-register.style.scss';
 
 const SingiAndRegister= () => (
     <div className="signin-n-register">
         <SignIn />
+        <SignUp />
     </div>
 )
 
