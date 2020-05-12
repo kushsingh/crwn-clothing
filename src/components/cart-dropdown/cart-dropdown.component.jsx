82e6da64f7919403c7c8969c4e5dce7eafe3165b
Kush Singh <kush.singh84@gmail.com>
2020-05-12 09:20:53 +0530

    updateing our application with reselect replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 import './cart-dropdown.style.scss';
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
     <div className="cart-dropdown">
-        <div className="cart-items"></div>
-        <CustomButton>Checkout</CustomButton>
+        <div className="cart-items">
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item= {cartItem} />
+            ))}
+        </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropDown;
+
+const mapDispatchToProps = state => ({
+    cartItems: selectCartItems(state) // used Memoization
+});
+
+export default connect(mapDispatchToProps)(CartDropDown);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types';
-import { cartItemToAdd } from './cart.utils'; 
+import { addItemToCart } from './cart.utils'; 
 
 const INITIAL_STATE = {
     hidden: true,
@@ -15,7 +15,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             case CartActionTypes.ADD_ITEM:
                 return {
                     ...state,
-                    cartItems: cartItemToAdd(state.cartItems, action.payload)
+                    cartItems: addItemToCart(state.cartItems, action.payload)
                 }
         default:
             return state
